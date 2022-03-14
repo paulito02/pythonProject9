6c49d8b4cc7060695a734cb1ba177682fb38a577
paulito02 <paul.makinde@yahoo.com>
2022-03-14 12:54:01 +0100

    getting highest even number

--- practice.py
+++ practice.py
@@ -1,40 +1,7 @@
-# ceating a dice game
-
-from random import randrange
-
-for i in range(0, 3):  # roll the die three times
-    value = randrange(1, 6)  # generate random number in the range 1....6
-# show the die
-print("+-------+")
-if value == 1:
-    print("|       |")
-    print("|   *   |")
-    print("|       |")
-elif value == 2:
-    print("| *     |")
-    print("|       |")
-    print("|     * |")
-elif value == 3:
-    print("|     * |")
-    print("|   *   |")
-    print("| *     |")
-elif value == 4:
-    print("| *   * |")
-    print("|       |")
-    print("| *   * |")
-elif value == 5:
-    print("| *   * |")
-    print("|   *   |")
-    print("| *   * |")
-elif value == 6:
-    print("| * * * |")
-    print("| * * * |")
-    print("| * * * |")
-else:
-    print(" *** error: illegal die value ***")
-print("+-------+")
-
-
-
-
-
+def highest_even(li):
+    evens = []
+    for x in li:
+        if x % 2 ==0:
+            evens.append(x)
+    return max(evens) 
+print(highest_even([2,10,2,3,4,8,11]))
