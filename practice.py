e150cbebb55da3a1a021e87a7350e47b44401bf5
paulito02 <paul.makinde@yahoo.com>
2022-03-15 19:09:17 +0100

    working on class and __init__ to build a mini profile for game ❤

--- practice.py
+++ practice.py
@@ -1,7 +1,43 @@
-def highest_even(li):
-    evens = []
-    for x in li:
-        if x % 2 ==0:
-            evens.append(x)
-    return max(evens) 
-print(highest_even([2,10,2,3,4,8,11]))
+# oop
+class PlayerCharacter:
+    #     class object attribute
+
+    def __init__(self, name, age):
+
+        self.name = name
+        if (age > 18):
+            self.age = age
+        else:
+            print("not up to age")
+
+    def run(self):
+        print(f'my name is {self.name}')
+
+
+print("             player 1")
+player1 = PlayerCharacter(input('name: '), int(input('age: ')))
+
+print("             player 2")
+player2 = PlayerCharacter(input('name: '), int(input('age: ')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
