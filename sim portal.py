2a23c19085e0e28d592ff858d4c99993b7d31cea
paulito02 <paul.makinde@yahoo.com>
2022-03-27 22:36:44 +0100

    working on a mini portal for registration of sim but you can help

---
+++ sim portal.py
@@ -0,0 +1,12 @@
+pin = input('input 4 digit pin you will like to use for your sim card')
+while len(pin) !=4:
+
+    if len(pin)==4:
+        pass
+    else:
+        len(pin) != 4
+        print("your input has to be 4 digit")
+        if len(input()) == 4:
+           break
+print("you are welcome")
+input("email :")
