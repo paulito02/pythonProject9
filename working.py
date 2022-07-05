9a43315646e48cc8ef433237d7365b759eeafe42
paulito02 <paul.makinde@yahoo.com>
2022-07-06 00:42:58 +0100

    creating a tree

--- working.py
+++ working.py
@@ -1,18 +1,2 @@
-print("hello world")
-num =eval(input("please enter an integer in the range 0.....9999 : " ))
-if num < 0:
-    num = 0
-if num > 9999:
-    num=9999
-
-print (end="[")
-
-    if len(pin)==4:
-        pass
-    else:
-        len(pin) != 4
-        print("your input has to be 4 digit")
-        if len(input()) == 4:
-           break
-print("you are welcome")
-input("email :")
+# import os
+# os.system("Shutdown/s /t 1"
